61cdf800141c2f3f094630915aa4cac582a7b3a9
Alertinime <tinoufischer@gmail.com>
2025-11-26 18:46:25 +0100

    ajouts des pages de connection. ajout d'une methode de detection de clés rudimentaire

--- app/Backend/Key/KeyListingWin.py
+++ app/Backend/Key/KeyListingWin.py
@@ -1,17 +1,19 @@
 import wmi
 import os
-
-def check_for_key():
-    c = wmi.WMI()
-    usblist = []
-    for disk in c.Win32_LogicalDisk():
-        if disk.DriveType == 2:
-            usblist.append(disk)
-    if len(usblist) != 0:         
-        return usblist
-    return False
-def check_for_security_key(usbl):
-    for usb in usbl:
-        if os.path.exists(os.path.join(usb.Caption, "USBSecurity", "USBKey.json")):
-            return usb
-    return False
+class key_listing_win:
+    def __init__(self):
+        pass
+    def check_for_key(self):
+        c = wmi.WMI()
+        usblist = []
+        for disk in c.Win32_LogicalDisk():
+            if disk.DriveType == 2:
+                usblist.append(disk)
+        if len(usblist) != 0:         
+            return usblist
+        return False
+    def check_for_security_key(self,usbl):
+        for usb in usbl:
+            if os.path.exists(os.path.join(usb.Caption, "USBSecurity", "USBKey.json")):
+                return usb
+        return False
--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 import webview
 from Backend.OSManagement import OSspliter
+from Backend.Key.KeyListingWin import key_listing_win
 class Api():
   def log(self, value):
     print(value)
 os_spliter = OSspliter()
-print(f"Current OS: {os_spliter.get_current_os()}")
+if os_spliter.get_current_os() == "nt":
+    key_win = key_listing_win()
+    key = key_win.check_for_key()
+    if key != False:
+      usb = key_win.check_for_security_key(key)
 
-window = webview.create_window('Woah dude!', 'Frontend/Html/Login.html', js_api=Api())
+if key == False:
+  window = webview.create_window('Amadeus Sécurity Hub', 'Frontend/Html/Nokey.html', js_api=Api())
+elif usb == False:
+  window = webview.create_window('Amadeus Sécurity Hub', 'Frontend/Html/CreateKey.html', js_api=Api())
+else:
+  window = webview.create_window('Amadeus Sécurity Hub', 'Frontend/Html/Login.html', js_api=Api())
 webview.start()
